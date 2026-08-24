7e25c538747a5870a8fb0f47e25d84755c9d5af2
zewireless <63487505+zewireless@users.noreply.github.com>
2026-08-24 14:08:32 +0800

    Update db.js

--- js/db.js
+++ js/db.js
@@ -192,9 +192,10 @@ const DB = (() => {
         return getStoreRows(table).length;
     }
 
-    function clear(table) {
-        cache[table] = (cache[table] || []).filter(r => r.storeId !== currentStoreId);
-    }
+function clear(table) {
+    cache[table] = (cache[table] || []).filter(r => r.storeId !== currentStoreId);
+    enqueueWrite('clear', table, null);
+}
 
     function resetAll() {
         TABLES.forEach(t => clear(t));
